9929e461f8b5f0e841a605c6146dbb79f9d557e4
RealAlphaUA <realalpha.ua@gmail.com>
2025-07-13 18:16:08 +0200

    feat: add KSP support with RabbitPacketProcessor and publishing configuration

---
+++ processor/src/main/kotlin/gg/levely/system/hoppermq/processor/RabbitPacketProcessor.kt
@@ -0,0 +1,55 @@
+package gg.levely.system.hoppermq.processor
+
+import com.google.devtools.ksp.processing.*
+import com.google.devtools.ksp.symbol.*
+
+class RabbitPacketProcessor(
+    private val codeGenerator: CodeGenerator,
+    private val logger: KSPLogger
+) : SymbolProcessor {
+
+    override fun process(resolver: Resolver): List<KSAnnotated> {
+        val symbols = resolver.getSymbolsWithAnnotation("gg.levely.system.hoppermq.core.packet.RabbitPacketLabel", true)
+            .filterIsInstance<KSClassDeclaration>()
+
+        if (!symbols.iterator().hasNext()) return emptyList()
+
+        val file = codeGenerator.createNewFile(
+            Dependencies(false),
+            packageName = "gg.levely.system.hoppermq.generated",
+            fileName = "GeneratedPacketRegistry"
+        )
+
+        file.bufferedWriter().use { writer ->
+            writer.write("package gg.levely.system.hoppermq.generated\n\n")
+            writer.write("import gg.levely.system.hoppermq.core.packet.PacketRegistry\n")
+
+            symbols.forEach {
+                val import = it.qualifiedName?.asString()
+                if (import != null) writer.write("import $import\n")
+            }
+
+            writer.write("\n")
+
+            writer.write("object GeneratedPacketRegistry {\n")
+            writer.write("\n")
+            writer.write("    fun registerAll(registry: PacketRegistry) {\n")
+
+            symbols.forEach {
+                val className = it.simpleName?.asString()
+                logger.warn("Registering RabbitPacket: $className")
+                if (className != null) {
+                    writer.write("        registry.register($className::class.java)\n")
+                }
+            }
+
+            writer.write("    }\n")
+
+            writer.write("\n")
+
+            writer.write("}\n")
+        }
+
+        return emptyList()
+    }
+}
---
+++ processor/src/main/kotlin/gg/levely/system/hoppermq/processor/RabbitPacketProcessorProvider.kt
@@ -0,0 +1,14 @@
+package gg.levely.system.hoppermq.processor
+
+import com.google.devtools.ksp.processing.SymbolProcessor
+import com.google.devtools.ksp.processing.SymbolProcessorEnvironment
+import com.google.devtools.ksp.processing.SymbolProcessorProvider
+
+class RabbitPacketProcessorProvider : SymbolProcessorProvider {
+
+    override fun create(environment: SymbolProcessorEnvironment): SymbolProcessor {
+        environment.logger.warn("RabbitPacketProcessorProvider is being created!")
+        return RabbitPacketProcessor(environment.codeGenerator, environment.logger)
+    }
+
+}
